99e8a88e865ca62a9b00e58578b77e35d3679c42
MacarenaGlez <macarenagonzalezalonso@gmail.com>
2021-11-25 16:47:27 +0100

    finish hamburguer menu

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const navToggle = document.querySelector(".js-nav-toggle");
+const navLinks = document.querySelectorAll(".js-nav__link");
+
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
